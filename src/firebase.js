c25069e4184e52463348e8ad0535667c465b68fd
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-12-20 09:37:35 +0700

    inline credential

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,12 @@
 import * as firebase from "firebase"
 
 const firebaseConfig = {
-  apiKey: process.env["FIREBASE_API_KEY"],
-  authDomain: process.env["FIREBASE_AUTH_DOMAIN"],
-  databaseURL: process.env["FIREBASE_DATABASE_URL"],
-  storageBucket: process.env["FIREBASE_STORAGE_BUCKET"],
+  apiKey: "AIzaSyD3uFdk8GC9P7NUjUI9Hh-EOaktybjUe-8",
+  authDomain: "routine-4d9fa.firebaseapp.com",
+  databaseURL: "https://routine-4d9fa.firebaseio.com",
+  projectId: "routine-4d9fa",
+  storageBucket: "routine-4d9fa.appspot.com",
+  messagingSenderId: "377339152317"
 }
 
 firebase.initializeApp(firebaseConfig)
