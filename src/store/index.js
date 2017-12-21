d5199ad9bebd6b895604d7f2e8ee49be876a8774
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-12-21 13:56:33 +0700

    redux-logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,19 @@
 import { createStore, applyMiddleware, compose } from "redux"
 import thunk from "redux-thunk"
+import logger form "redux-logger"
 import { autoRehydrate, persistStore } from "redux-persist"
 
 import rootReducers from "../reducers"
 
-const middlewares = applyMiddleware(thunk)
+let store = compose(
+  applyMiddleware(
+    thunk,
+    logger,
+  ),
+  autoRehydrate(),
+)(createStore)(rootReducers)
 
-let store = createStore(
-  rootReducers,
-  compose(
-    middlewares,
-    autoRehydrate(),
-  )
-)
-// const store = createStore(rootReducers, {}, middlewares)
+// const store = createStore(rootReducers, middlewares)
 
 persistStore(store)
 export default store
