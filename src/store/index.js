c50977fe8a185bb9ea6cc7ed28936ad69ff52dce
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-12-21 12:28:06 +0700

    add redux-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,19 @@
-import { createStore, applyMiddleware } from "redux"
+import { createStore, applyMiddleware, compose } from "redux"
 import thunk from "redux-thunk"
+import { autoRehydrate, persistStore } from "redux-persist"
 
 import rootReducers from "../reducers"
 
 const middlewares = applyMiddleware(thunk)
 
-const store = createStore(rootReducers, {}, middlewares)
+let store = createStore(
+  rootReducers,
+  compose(
+    middlewares,
+    autoRehydrate(),
+  )
+)
+// const store = createStore(rootReducers, {}, middlewares)
 
+persistStore(store)
 export default store
